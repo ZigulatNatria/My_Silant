da45b5f5ae097b772cbefc0bd63845558c706e88
ZigulatNatria <96946885+ZigulatNatria@users.noreply.github.com>
2022-12-24 16:45:30 +0500

    Добавленны таблицы списков для полей основных моделей

--- my_silant/service/admin.py
+++ my_silant/service/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Machine, TO, Complaint
+#Модели справочников
+from .models import ServiceCompany, TechniqueModel, EngineModel, TransmissionModel, \
+    DriveAxleModel, SteeringBridgeModel, ServiceType, FailureNode, RecoveryMethod
 # Register your models here.
 admin.site.register(Machine)
 admin.site.register(TO)
 admin.site.register(Complaint)
+
+# Модели справочников
+admin.site.register(ServiceCompany)
+admin.site.register(TechniqueModel)
+admin.site.register(TransmissionModel)
+admin.site.register(DriveAxleModel)
+admin.site.register(SteeringBridgeModel)
+admin.site.register(ServiceType)
+admin.site.register(FailureNode)
+admin.site.register(RecoveryMethod)
+admin.site.register(EngineModel)
--- my_silant/service/models.py
+++ my_silant/service/models.py
@@ -1,55 +1,162 @@
 from django.db import models
 
-# Create your models here.
+class ServiceCompany(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Сервисная компания'
+
+
+class TechniqueModel(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель техники'
+
+
+class EngineModel(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель двигателя'
+
+
+class TransmissionModel(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель трансмиссии'
+
+
+class DriveAxleModel(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель ведущего моста'
+
+
+class SteeringBridgeModel(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Модель управляемого моста'
+
+
 class Machine(models.Model):
     number_machine = models.TextField(unique=True, verbose_name='Зав. № машины')
-    technique_model = models.TextField(verbose_name='Модель техники') #TODO добавить справочник
-    engine_model = models.TextField(verbose_name='Модель двигателя') #TODO добавить справочник
+    technique_model = models.OneToOneField(TechniqueModel, verbose_name='Модель техники', on_delete=models.CASCADE)
+    engine_model = models.OneToOneField(EngineModel, verbose_name='Модель двигателя', on_delete=models.CASCADE)
     engine_number = models.TextField(verbose_name='Зав. № двигателя')
-    transmission_model = models.TextField(verbose_name='Модель трансмиссии') #TODO добавить справочник
+    transmission_model = models.OneToOneField(TransmissionModel, verbose_name='Модель трансмиссии', on_delete=models.CASCADE)
     transmission_number = models.TextField(verbose_name='Зав. № трансмиссии')
-    drive_axle_model = models.TextField(verbose_name='Модель ведущего моста') #TODO добавить справочник
+    drive_axle_model = models.OneToOneField(DriveAxleModel, verbose_name='Модель ведущего моста', on_delete=models.CASCADE)
     drive_axle_number = models.TextField(verbose_name='Зав. № ведущего моста')
-    steering_bridge_model = models.TextField(verbose_name='Модель управляемоо моста')  #TODO добавить справочник
-    steering_bridge_number = models.TextField(verbose_name='Зав. № управляемоо моста')
+    steering_bridge_model = models.OneToOneField(SteeringBridgeModel, verbose_name='Модель управляемого моста', on_delete=models.CASCADE)
+    steering_bridge_number = models.TextField(verbose_name='Зав. № управляемого моста')
     supply_contract = models.TextField(verbose_name='Договор поставки №, дата')
     shipping_date = models.DateField(verbose_name='Дата отгрузки с завода') #TODO календарь
     consignee = models.TextField(verbose_name='Грузополучатель (конечный потребитель)')
     delivery_address = models.TextField(verbose_name='Адрес поставки (эксплуатации)')
     equipment = models.TextField(verbose_name='Комплектация (доп. опции)')
     client = models.TextField(verbose_name='Клиент') #TODO справочник пользователей с соответствующими правами
-    service_company = models.TextField(verbose_name='Сервисная компания') #TODO справочник пользователей с соответствующими правами
+    # service_company = models.TextField(verbose_name='Сервисная компания') #TODO справочник пользователей с соответствующими правами
+    service_company = models.ForeignKey(ServiceCompany, verbose_name='Сервисная компания', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number_machine
 
     class Meta:
         verbose_name = 'Машина'
 
+class ServiceType(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Вид ТО'
+
 
 class TO(models.Model):
-    service_type = models.TextField(verbose_name='Вид ТО') #TODO добавить справочник
+    service_type = models.OneToOneField(ServiceType, verbose_name='Вид ТО', on_delete=models.CASCADE)
     service_date = models.DateField(verbose_name='Дата проведения ТО') #TODO календарь
     operating_time = models.FloatField(verbose_name='Наработка, м/час')
     work_order_number = models.TextField(verbose_name='№ заказ-наряда')
     work_order_date = models.DateField(verbose_name='дата заказ-наряда')  # TODO календарь
-    company_make_service = models.TextField(verbose_name='Организация, проводившая ТО') #TODO добавить справочник
+    company_make_service = models.ForeignKey(ServiceCompany, on_delete=models.CASCADE)
+    # company_make_service = models.TextField(verbose_name='Организация, проводившая ТО') #TODO добавить справочник
     machine_to = models.ForeignKey(Machine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.work_order_number
 
     class Meta:
         verbose_name = '«ТО» (техническое обслуживание)'
 
 
+class FailureNode(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Характер отказа'
+
+
+class RecoveryMethod(models.Model):
+    name = models.TextField(verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Способ восстановления'
+
+
 class Complaint(models.Model):
     date_rejection = models.DateField(verbose_name='Дата отказа') #TODO календарь
     operating_time = models.FloatField(verbose_name='Наработка, м/час')
-    failure_node = models.TextField(verbose_name='Узел отказа') #TODO добавить справочник
+    failure_node = models.OneToOneField(FailureNode, verbose_name='Узел отказа', on_delete=models.CASCADE)
     failure_description = models.TextField(verbose_name='Описание отказа')
-    recovery_method = models.TextField(verbose_name='Способ восстановления') #TODO добавить справочник
+    recovery_method = models.OneToOneField(RecoveryMethod, verbose_name='Способ восстановления', on_delete=models.CASCADE)
     spare_parts = models.TextField(verbose_name='Используемые запасные части')
     recovery_date = models.DateField(verbose_name='Дата восстановления') #TODO календарь
-    # downtime = models.ImageField(recovery_date - date_rejection, verbose_name='Время простоя техники')
     machine_complaint = models.ForeignKey(Machine, on_delete=models.CASCADE)
 
     def downtime(self):
         return (self.recovery_date - self.date_rejection).days
+
+    def __str__(self):
+        return self.failure_description
 
     class Meta:
         verbose_name = 'Рекламации'
